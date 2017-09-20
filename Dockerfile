0955c01429f9228a59f0bb58dff73d94140b5236
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-09-19 22:23:45 -0300

    Fix ports

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ADD . /usr/src/app
 WORKDIR /usr/src/app
 
 ENV NODE_ENV production
+ENV IS_DOCKER ISDOCKERTRUE
 
 EXPOSE 3000
 
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as http from 'http';
 
 module.exports = http.createServer(function (req, res) {
-  res.end('hello world');
-}).listen(process.env.PORT, () => console.log("NodeJS worker listening on port 8080"));
+  res.end('hello world ' + process.env.IS_DOCKER);
+}).listen(process.env.PORT, () => console.log(`NodeJS worker listening on port ${process.env.PORT}`));
