48ab837558c0e843ccdad72166bc6cbfc1c7b0d0
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-09-19 22:49:56 -0300

    Is this docker? log

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ console.log('is this docker?', process.env.IS_DOCKER);
 
 module.exports = http.createServer(function (req, res) {
   res.end('hello world ' + process.env.IS_DOCKER);
-}).listen(process.env.PORT || 3000, () => console.log(`NodeJS worker listening on port ${process.env.PORT}`));
+}).listen(process.env.PORT || 3000, () => console.log(`NodeJS worker listening on port ${process.env.PORT} -- ${process.env.IS_DOCKER}`));
