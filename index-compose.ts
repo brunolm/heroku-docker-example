78355358cc120454e048d5d1f2d1bd2ce283fb60
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-09-19 22:37:13 -0300

    Stuff

--- index-compose.ts
+++ index-compose.ts
@@ -2,4 +2,4 @@ import * as http from 'http';
 
 module.exports = http.createServer(function (req, res) {
   res.end('This was started from docker-compose!');
-}).listen(process.env.PORT, () => console.log(`NodeJS worker listening on port ${process.env.PORT}`));
+}).listen(process.env.PORT || 3000, () => console.log(`NodeJS worker listening on port ${process.env.PORT}`));
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import * as http from 'http';
 
+console.log('is this docker?', process.env.IS_DOCKER);
+
 module.exports = http.createServer(function (req, res) {
   res.end('hello world ' + process.env.IS_DOCKER);
-}).listen(process.env.PORT, () => console.log(`NodeJS worker listening on port ${process.env.PORT}`));
+}).listen(process.env.PORT || 3000, () => console.log(`NodeJS worker listening on port ${process.env.PORT}`));
